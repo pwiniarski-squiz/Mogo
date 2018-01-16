c71ac025105b0541982d1aa79b88f2273023c3dd
Piotr <>
2018-01-16 09:29:40 +0100

    publishing github project

---
+++ docs/js/global.js
@@ -0,0 +1,35 @@
+/*global jQuery*/
+/**
+ * mogo
+ * Global JS
+ *
+ * version: 0.0.1
+ * file:    global.js
+ * author:  Squiz Australia
+ * change log:
+ *     Fri Dec 23 2016 13:36:53 GMT+0100 (Środkowoeuropejski czas stand.) - First revision
+ */
+
+/*
+ * Table of Contents
+ *
+ * - Global
+ * - Modules
+
+ */
+'use strict';
+/*
+--------------------
+Global
+--------------------
+*/
+//  Declare JS Enabled.
+
+$('body').removeClass('no-js').addClass('js-enabled');
+
+/*
+--------------------
+Modules
+--------------------
+*/
+//# sourceMappingURL=global.js.map
